d7973ec81a1723ef4a63305b31ea025d01d82674
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-12-01 13:01:50 -0600

    slightly better readability

--- sonarSweep/index.js
+++ sonarSweep/index.js
@@ -22,9 +22,8 @@ let sonarSweep2 = (readout) => {
   if (depths.length < 4) return 0;
   let count = 0;
   let prevSum = depths[0] + depths[1] + depths[2];
-  for (let right = 3; right < depths.length; right++ ) {
-    let left = right - 2;
-    let curSum = prevSum - depths[left - 1] + depths[right];
+  for (let i = 3; i < depths.length; i++ ) {
+    let curSum = depths[i] + depths[i - 1] + depths[i - 2];
     if (curSum > prevSum) {
       count++;
     }
