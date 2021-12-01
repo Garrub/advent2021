a2f54e8c31ee7048351d880f964d4e8dd4a83958
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-12-01 00:14:14 -0600

    day 1 part 2

--- sonarSweep/index.js
+++ sonarSweep/index.js
@@ -1,7 +1,9 @@
 import input from "./input.js";
 
+let parse = (list) => list.trim().split('\n').map(depth => Number(depth));
+
 let sonarSweep = (readout) => {
-  let depths = readout.trim().split('\n').map(depth => Number(depth));
+  let depths = parse(readout);
   let count = 0;
   for (let i = 1; i < depths.length; i++) {
     let depth = depths[i];
@@ -14,3 +16,21 @@ let sonarSweep = (readout) => {
 };
 
 console.log('part 1: ', sonarSweep(input));
+
+let sonarSweep2 = (readout) => {
+  let depths = parse(readout);
+  if (depths.length < 4) return 0;
+  let count = 0;
+  let prevSum = depths[0] + depths[1] + depths[2];
+  for (let right = 3; right < depths.length; right++ ) {
+    let left = right - 2;
+    let curSum = prevSum - depths[left - 1] + depths[right];
+    if (curSum > prevSum) {
+      count++;
+    }
+    prevSum = curSum;
+  }
+  return count;
+};
+
+console.log('part 2: ', sonarSweep2(input));
