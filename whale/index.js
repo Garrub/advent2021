fc2200b7a710c295aaf6ae93c1089cbdc4684a5c
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-12-07 13:51:17 -0600

    day 7 part 2, is bad

--- whale/index.js
+++ whale/index.js
@@ -8,13 +8,26 @@ const getMedian = list => {
   return sorted.length % 2 === 1 ?
     sorted[index] :
     (sorted[Math.ceil(index)] + sorted[Math.floor(index)]) / 2;
-}
+};
 
-const getAlignmentFuel = raw => {
+const getMean = list => {
+  //ceil to pass test input, floor for real input. IDK why. maybe bad float math?
+  return Math.floor(list.reduce((sum, cur) => sum + cur) / list.length);
+};
+
+const getFuel1 = (crabs, level) => crabs.reduce((dist, cur) => dist + Math.abs(level - cur), 0)
+const getFuel2 = (crabs, level) => crabs.reduce((dist, cur) => {
+  const diff = Math.abs(level - cur)
+  return dist + (diff * (diff + 1)) / 2
+}, 0)
+
+const getAlignmentFuel = (raw, getLevel, getFuel) => {
   crabPos = raw.split(',').map(x => parseInt(x));
-  const median = getMedian(crabPos);
-  return crabPos.reduce((dist, cur) => dist + Math.abs(median - cur), 0);
-}
+  const level = getLevel(crabPos);
+  return getFuel(crabPos, level);
+};
 
-console.log('should be 37: ', getAlignmentFuel(test));
-console.log('part 1: ', getAlignmentFuel(input));
+console.log('should be 37: ', getAlignmentFuel(test, getMedian, getFuel1));
+console.log('part 1: ', getAlignmentFuel(input, getMedian, getFuel1));
+console.log('should be 168: ', getAlignmentFuel(test, getMean, getFuel2));
+console.log('part 2: ', getAlignmentFuel(input, getMean, getFuel2));
